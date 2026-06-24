6a3a8eb29920657d1b608b52e36ea22cd517ce73
ktstovega-cmyk <orinamannami@mannamiorinashinoMacBook-Pro.local>
2026-06-24 16:56:04 +0800

    edit controller

--- resources/views/users/messages/create.blade.php
+++ resources/views/users/messages/create.blade.php
@@ -1,5 +1,25 @@
-<form action="#" method="post">
-    <div class="input-group">
-        <input type="text" name="message" id="message" class="form-control">
+@extends('layouts.app')
+ 
+@section('title', 'Create New Message')
+ 
+@section('content')
+    <div class="container w-50 mx-auto">
+        <form action="#" method="post">
+            @csrf
+            <div class="input-group">
+                <select name="receiver" id="receiver" class="form-select">
+                    <option value="" selected disabled>To ...</option>
+                    @foreach ($friend_users as $user)
+                        <option value="{{$user->id}}">{{$user->name}}</option>
+                    @endforeach
+                </select>
+            </div>
+            <div class="input-group mt-3">
+                <input type="text" name="message" id="message" class="form-control form-control-sm" placeholder="new message...">
+                <button type="submit" class="btn btn-outline-primary btn-sm" title="Send Message"><i class="fa-solid fa-arrow-right"></i></button>
+            </div>
+        </form>
+
     </div>
-</form>
+@endsection
+ 
